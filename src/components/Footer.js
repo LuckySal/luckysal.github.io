855efe2748a3ecf4a6348143f6492a4dbba4ea3d
Brandon <hillbp2014@gmail.com>
2022-12-26 11:17:21 -0500

    added fontawesome icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+
 export default function Footer() {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <a className="icon" href="https://github.com/LuckySal">
+        <FontAwesomeIcon icon={brands("github")} />
+      </a>
+      <a
+        className="icon"
+        href="https://www.linkedin.com/in/brandon-hill-74a756154/">
+        <FontAwesomeIcon icon={brands("linkedin")} />
+      </a>
+      <a className="icon" href="mailto:dev@luckysal.com">
+        <FontAwesomeIcon icon={regular("envelope")} />
+      </a>
+    </footer>
   );
 }
