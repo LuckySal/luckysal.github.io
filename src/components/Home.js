298f19e4a08a9e5dd93de80d4c2582dd8957ef32
Brandon <hillbp2014@gmail.com>
2022-12-26 10:20:20 -0500

    carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
 
-import Jumbotron from "./Jumbotron";
+import Carousel from "react-bootstrap/Carousel";
+
+import Image1 from "../assets/images/image-1.jpg";
+import Image2 from "../assets/images/image-2.jpg";
+import Image3 from "../assets/images/image-3.jpg";
 
 export default function Home() {
   return (
     <div>
-      <Jumbotron />
+      <Carousel
+        controls={false}
+        indicators={false}
+        keyboard={false}
+        pause={false}
+        touch={false}>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Image1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Image2} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Image3} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
       <p>Home</p>
     </div>
   );
