195f292bfc3b2e2b788f9276b733c7875b4a6136
Brandon <hillbp2014@gmail.com>
2022-12-26 10:20:32 -0500

    fixed logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,16 @@ import logo from "../logo.svg";
 
 function Header() {
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Brandon Hill</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            style={{ height: 50, width: 50 }}
+            alt="website logo"
+          />
+          Brandon Hill
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
