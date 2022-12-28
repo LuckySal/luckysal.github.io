4449e0233e26a9f8b4a6e694ccc6f13355cb70f5
Brandon <hillbp2014@gmail.com>
2022-12-28 16:08:34 -0500

    added photo to about-me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./AboutMe.css";
 
+import Selfie from "../../assets/images/me.png";
+
 export default function AboutMe() {
   return (
-    <Container>
+    <Container id="about-me">
       <main>
-        <p>
-          Hello! My name is Brandon Hill. I'm a Full-Stack Web Developer from
-          Florida.
-        </p>
+        <img src={Selfie} />
+        <div>
+          <p>
+            Hello! My name is Brandon Hill. I'm a Full-Stack Web Developer from
+            Florida.
+          </p>
+        </div>
       </main>
     </Container>
   );
