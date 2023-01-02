b0f79a5a9c1b66aa85ff4aa319ff649695db2f00
Brandon <hillbp2014@gmail.com>
2023-01-02 12:07:18 -0500

    style changes for homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ import Image3 from "../../assets/images/image-3.jpg";
 
 export default function Home() {
   return (
-    <div>
+    <div className="position-relative">
       <Carousel
         controls={false}
         indicators={false}
@@ -27,7 +27,7 @@ export default function Home() {
           <img className="d-block w-100" src={Image3} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
-      <p>Home</p>
+      <p id="home-text" className="position-absolute top-50 start-50 translate-middle w-75">I am a Full-Stack Web Developer from Vero Beach, Florida. I received my certificate from the University of Central Florida in December of 2022. I am looking to begin a career in web development.</p>
     </div>
   );
 }
