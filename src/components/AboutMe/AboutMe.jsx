36d143cffbd4d2d422f3cca7eb4412ad1b6a4325
Brandon <hillbp2014@gmail.com>
2023-01-20 13:43:38 -0500

    added badge

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,13 +5,21 @@ import { Container, Image, Row, Col } from "react-bootstrap";
 import "./AboutMe.css";
 
 import Selfie from "../../assets/images/me.png";
+import Badge from "../../assets/images/badge.png";
 
 export default function AboutMe() {
   return (
     <Container id="about-me">
       <Row>
         <Col className="about-me" sm={4}>
-          <Image src={Selfie} alt="Brandon Hill" fluid={true} rounded={true}/>
+          <Image src={Selfie} alt="Brandon Hill" fluid={true} rounded={true} />
+          <a
+            className="icon px-3"
+            href="https://api.badgr.io/public/assertions/cVgk6Z03Tw2PCSofrSM3sA?identity__email=hillbp2014%40gmail.com"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={Badge} alt="Coding boot camp badge" width="400" height="500"/>
+          </a>
         </Col>
         <Col className="about-me" sm={8}>
           <p>
